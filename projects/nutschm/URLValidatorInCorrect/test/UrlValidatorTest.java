3573e538816c8f3380fcf8aec35a3d4048a170d6
vchapple17 <vchapple17@gmail.com>
2018-03-13 10:37:39 -0500

    basic manual test added. Fails to report any as valid.

--- projects/nutschm/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/nutschm/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,22 +7,54 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
-
 
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
-   
+ 
    
    public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
+   {		//You can use this function to implement your manual testing
 	   
+//	   String[] schemes = {"http","https"};
+//	   UrlValidator urlVal = new UrlValidator(schemes, 0);
+	   UrlValidator urlVal = new UrlValidator();
+	   // Test Data
+	   String[] testURLFalse = {
+			   "fake:/oregonstate.edu/",
+			   "bad://www.google.com/",
+			   "htttp://soc/",
+			   "bad://www.google.com/asdf/as/fd/",
+			   "htttp://soc/1.",
+			   "htttp://1.2.2./path?.com."
+			   
+	   };
+	   String[] testURLTrue = {
+			   "http://oregonstate.edu/",
+			   "https://www.google.com/",
+			   "http://ecampus.oregonstate.edu/forms/degree-comparison.htm"
+	   };
+	   
+	   int i ;
+	   for (i = 0; i < testURLFalse.length; i++ ) {
+		   boolean result = urlVal.isValid( testURLFalse[i]);
+		   if (result == false) {
+			   System.out.print('.');
+		   } else {
+			   System.out.print('X');
+		   }
+	   }
+	   
+	   for (i = 0; i < testURLTrue.length; i++ ) {
+		   boolean result = urlVal.isValid( testURLTrue[i]);
+		   if (result == true) {
+			   System.out.print('.');
+		   } else {
+			   System.out.print('X');
+		   }
+	   }
    }
    
    
